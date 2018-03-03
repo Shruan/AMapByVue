a5dd1849e904dd9d8faf6619688930237f278f66
Shruan <qiushiyuan1994@qq.com>
2018-03-04 00:56:44 +0800

    引入axios

--- src/main.js
+++ src/main.js
@@ -4,11 +4,18 @@ import Vue from 'vue'
 import App from './App'
 import router from './router'
 import iView from 'iview'
+import axios from 'axios'
 import 'iview/dist/styles/iview.css'
 
 Vue.use(iView)
 
 Vue.config.productionTip = false
+
+Vue.prototype.$http = axios
+Vue.prototype.$qs = require('querystring').stringify
+Vue.prototype.$header = {
+  'Content-Type': 'application/json;charset=utf-8'
+}
 
 /* eslint-disable no-new */
 new Vue({
